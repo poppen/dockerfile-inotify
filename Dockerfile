4ff00fc0c52a3d3c3c83b7b82f3400edc13376a4
poppen <poppen.jp@gmail.com>
2018-05-15 18:45:18 +0900

    feat: change to call scripts from entrypoint.sh instead of eval string

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 FROM alpine:latest
 
+COPY entrypoint.sh /
+COPY run.d/ /run.d/
+
 # hadolint ignore=DL3018
 RUN apk --no-cache add \
         tini \
-        inotify-tools
-COPY entrypoint.sh /
-RUN chmod 0755 /entrypoint.sh
+        inotify-tools && \
+    chmod 0755 /entrypoint.sh /run.d/*
 
 ENTRYPOINT ["tini", "--", "/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,19 @@ INOTIFY_EVENTS=${INOTIFY_EVENTS:-"create,delete,modify,move"}
 INOTIFY_OPTONS=${INOTIFY_OPTONS:-"-r"}
 INOTIFY_EXCLUDE_PATTERN=${INOTIFY_EXCLUDE_PATTERN:-"\.sw[pox]$|.*~$|.*\.bak$|\.git/.*"}
 INOTIFY_OUTPUT_FORMAT=${INOTIFY_OUTPUT_FORMAT:-"%w%f"}
-INOTIFY_COMMAND=${INOTIFY_COMMAND:-'echo $FILE'}
 VOLUMES=${VOLUMES:-"/data"}
 
 inotifywait -e "${INOTIFY_EVENTS}" -m ${INOTIFY_OPTONS} --exclude="${INOTIFY_EXCLUDE_PATTERN}" "${VOLUMES}" --format "${INOTIFY_OUTPUT_FORMAT}" | \
-while read -r FILE; do
-    eval "${INOTIFY_COMMAND}"
-done
+    while read -r FILE; do
+        if [ -d /run.d ]; then
+            for f in /run.d/*; do
+                if [ -x "$f" ]; then
+                    echo "Running $f ..."
+                    $f "$FILE"
+                else
+                    echo "Could not run $f, because it's missing execute permission (+x)." >&2
+                fi
+            done
+            unset f
+        fi
+    done
